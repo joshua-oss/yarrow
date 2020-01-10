3d729762775e9d65d8373e7a56be1a31fb6eca1a
Christian Covington <christian.t.covington@gmail.com>
2020-01-10 10:00:58 -0500

    first attempt at stability-based histogram

--- runtime-rust/src/algorithms.rs
+++ runtime-rust/src/algorithms.rs
@@ -98,9 +98,41 @@ pub fn dp_histogram_laplace(
     // calculate non-private histogram
     let mut hist = nonprivate_functions::histogram(clamped_data, minimum, maximum, num_bins, inclusive_left);
 
-    // add laplace noise to each bin
-    for (bin, count) in hist.iter_mut() {
+    // add laplace noise to each bin's count
+    for (_, count) in hist.iter_mut() {
         *count += noise::sample_laplace(0., sensitivity / epsilon);
+    }
+
+    // return DP histogram
+    return hist;
+}
+
+pub fn dp_histogram_stability(
+    epsilon: f64, delta: f64, data: ArrayD<f64>,
+    minimum: f64, maximum: f64,
+    num_bins: usize, inclusive_left: bool) -> HashMap::<String, f64> {
+
+    // set sensitivity
+    // NOTE: The sensitivity is set at 2 because we consider releasing the entire vector of counts at once.
+    //       Changing one data point can change the counts of two bins, each by at most one.
+    //       Thus, the maximum difference in l1 norm is 2.
+    let sensitivity: f64 = 2.0;
+
+    // clamp data
+    let clamped_data: ArrayD<f64> = data.mapv(|v| num::clamp(v, minimum, maximum));
+
+    // calculate non-private histogram
+    let mut hist = nonprivate_functions::histogram(clamped_data, minimum, maximum, num_bins, inclusive_left);
+
+    // add laplace noise to each bin's count that is not 0
+    // then, check threshold and set count to 0 if it is too small
+    for (_, count) in hist.iter_mut() {
+        if *count != 0.0 {
+            *count += noise::sample_laplace(0., sensitivity / epsilon);
+            if *count < 2.0*(2.0 / delta).ln() + 1.0 {
+                *count = 0.0;
+            }
+        }
     }
 
     // return DP histogram
